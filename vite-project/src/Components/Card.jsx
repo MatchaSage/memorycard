c14b913d303ecc9f21d75fc9ddb7607aa5696f9b
Treyce Wheeler <t.zeryon@gmail.com>
2023-05-17 10:46:30 -0400

    initial commit

---
+++ vite-project/src/Components/Card.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Card(props) {
+  return (
+    <div className="card-container">
+      <img className="card--image"></img>
+      <p className="card--title"></p>
+    </div>
+  );
+}
---
+++ vite-project/src/Components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <h1 className="header--text"></h1>
+      <span className="header--scoreBoard"></span>
+    </div>
+  );
+}
